cb117ac10f187374093f953cb9138a7899f67708
aadityakalwani <aadityakalwani@gmail.com>
2021-08-03 10:16:55 +0100

    managed to make raycast work (cmd+space)

--- main.py
+++ main.py
@@ -11,16 +11,21 @@ import pyautogui
 # >>> pyautogui.click() # Click the mouse at its current location.
 # >>> pyautogui.click(200, 220) # Click the mouse at the x, y coordinates 200, 220.
 # >>> pyautogui.move(None, 10)  # Move mouse 10 pixels down, that is, move the mouse relative to its current position.
-# >>> pyautogui.doubleClick() # Double click the mouse at the
+# >>> pyautogui.doubleClick() # Double click the mouse
 
 # >>> pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)
 # Use tweening/easing function to move mouse over 2 seconds.
 
-# >>> pyautogui.write('Hello world!', interval=0.25)  # Type with quarter-second pause in between each key.
+# >>> pyautogui.write('Hello world!', interval=0.25)
+# Type with quarter-second pause in between each key.
+
 # >>> pyautogui.press('esc') # Simulate pressing the Escape key.
 # >>> pyautogui.keyDown('shift')
 # >>> pyautogui.write(['left', 'left', 'left', 'left', 'left', 'left'])
 # >>> pyautogui.keyUp('shift')
 # >>> pyautogui.hotkey('ctrl', 'c')
 
-pyautogui.write('Hello world!', interval=0.)
+pyautogui.keyDown("command")
+pyautogui.press("space")
+pyautogui.write('Hello world!', interval=1)
+
